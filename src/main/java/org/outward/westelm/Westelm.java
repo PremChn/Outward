c7cd0e0aa676808beb197d43d11395ca58de10dd
Premanand Paneerselvan (contractor) <YH01502@federated.fds>
2018-11-07 14:38:06 -0800

    made some changes to handle stale object error

--- src/main/java/org/outward/westelm/Westelm.java
+++ src/main/java/org/outward/westelm/Westelm.java
@@ -8,10 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.DataProvider;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -25,7 +23,7 @@ public class Westelm {
         co.addArguments("start-maximized");
         System.setProperty("webdriver.chrome.driver", "C:\\Prem\\outward\\Outward\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(co);
-        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebDriverWait wait = new WebDriverWait(driver, 50);
 
         /*System.setProperty("webdriver.gecko.driver.driver","./");
         WebDriver driver = new FirefoxDriver();
@@ -36,36 +34,35 @@ public class Westelm {
         if (driver.getTitle().contains("west elm")) {
 
             //to close the pop-up
-            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"home\"]/div[7]/div/a[1]")));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"home\"]/div[7]/div/a[1]")));
             driver.findElement(By.xpath("//*[@id=\"home\"]/div[7]/div/a[1]")).click();
 
-            WebElement searchField = driver.findElement(By.id("search-field"));
-
             //to enter the sku number in search field and validate the valid SKU
-           /* searchField.sendKeys("2613243");
-            searchField.sendKeys(Keys.RETURN);
-            //System.out.println(driver.getCurrentUrl());
-            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#pip div.jump-below > div > a")));
-            WebElement addToCartBtn = driver.findElement(By.cssSelector("div#pip div.jump-below > div > a"));
-            if (addToCartBtn.isDisplayed()) {
+            driver.findElement(By.id("search-field")).sendKeys("2613243");
+            driver.findElement(By.id("search-field")).sendKeys(Keys.RETURN);
+            wait.until(ExpectedConditions
+                               .visibilityOfElementLocated(By.cssSelector("div#pip div.jump-below > div > a")));
+
+            if (driver.findElement(By.cssSelector("div#pip div.jump-below > div > a")).isDisplayed()) {
                 System.out.println("Valid UPC - User able to see the result");
             }
             //Assert.assertTrue(addToCartBtn.isDisplayed(),"Invalid UPC - User is not able to see the result");
 
 
-            //to enter the sku number in search field and validate the invalid SKU
-            searchField.sendKeys("12345");
-            searchField.sendKeys(Keys.RETURN);
-            WebElement invalidUPCErrorMsg = driver.findElement(By.xpath("//*[@id=\"content\"]/ul"));
-            if (invalidUPCErrorMsg.isDisplayed()) {
+            //to enter the sku number in search field and validate invalid SKU
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-field")));
+            driver.findElement(By.id("search-field")).sendKeys("12345");
+            driver.findElement(By.id("search-field")).sendKeys(Keys.RETURN);
+            if (driver.findElement(By.xpath("//*[@id=\"content\"]/ul")).isDisplayed()) {
                 System.out.println("Invalid UPC - User not able to see the result");
             }
             //Assert.assertTrue(invalidUPCErrorMsg.isDisplayed(),"Valid UPC - Please provide invalid UPC to validate
-            // this scenario");*/
+            // this scenario");
 
             //to find stores for states "Arizona" and "Texas"
             driver.findElement(By.id("stores-link")).click();
-            driver.findElement(By.xpath("//*[@id=\"store-locator\"]/main/div[3]/div[2]/section[1]/section[1]/div[3]/a")).click();
+            driver.findElement(By.xpath("//*[@id=\"store-locator\"]/main/div[3]/div[2]/section[1]/section[1]/div[3]/a"))
+                  .click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("state-list-selector")));
             Select stateDropDown = new Select(driver.findElement(By.id("state-list-selector")));
             stateDropDown.selectByVisibleText("Arizona");
@@ -80,7 +77,7 @@ public class Westelm {
 
         }
 
-        //driver.close();
+        driver.quit();
 
     }
 }
